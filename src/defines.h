bce710a391628438b93699cdc5199b8eae7930db
McCoy2X <federicobeuter@gmail.com>
2015-05-21 20:39:10 -0300

    Cosas.

---
+++ src/defines.h
@@ -0,0 +1,55 @@
+/* ** por compatibilidad se omiten tildes **
+================================================================================
+ TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+================================================================================
+
+    Definiciones globales del sistema.
+*/
+
+#ifndef __DEFINES_H__
+#define __DEFINES_H__
+
+/* Tipos basicos */
+/* -------------------------------------------------------------------------- */
+#define NULL                    0
+#define TRUE                    0x00000001
+#define FALSE                   0x00000000
+#define ERROR                   1
+
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+typedef unsigned int   uint;
+
+/* Constantes basicas */
+/* -------------------------------------------------------------------------- */
+#define PAGE_SIZE               0x00001000
+#define TASK_SIZE               4096
+
+#define BOOTSECTOR              0x00001000 /* direccion fisica de comienzo del bootsector (copiado) */
+#define KERNEL                  0x00001200 /* direccion fisica de comienzo del kernel */
+
+
+/* Indices en la gdt */
+/* -------------------------------------------------------------------------- */
+#define GDT_COUNT 30
+
+#define GDT_IDX_NULL_DESC           0
+#define GDT_NIVEL0_CODIGO           8
+#define GDT_NIVEL0_DATOS            9
+#define GDT_NIVEL3_CODIGO           10
+#define GDT_NIVEL3_DATOS            11
+#define GDT_PANTALLA            	12
+
+/* Offsets en la gdt */
+/* -------------------------------------------------------------------------- */
+#define GDT_OFF_NULL_DESC           (GDT_IDX_NULL_DESC      << 3)
+
+/* Selectores de segmentos */
+/* -------------------------------------------------------------------------- */
+
+void* error();
+
+#define ASSERT(x) while(!(x)) {};
+
+
+#endif  /* !__DEFINES_H__ */
---
+++ src/kernel.asm
@@ -0,0 +1,108 @@
+; ** por compatibilidad se omiten tildes **
+; ==============================================================================
+; TRABAJO PRACTICO 3 - System Programming - ORGANIZACION DE COMPUTADOR II - FCEN
+; ==============================================================================
+
+%include "imprimir.mac"
+
+global start
+
+extern GDT_DESC
+
+;; Saltear seccion de datos
+jmp start
+
+;;
+;; Seccion de datos.
+;; -------------------------------------------------------------------------- ;;
+iniciando_mr_msg db     'Iniciando kernel (Modo Real)...'
+iniciando_mr_len equ    $ - iniciando_mr_msg
+
+iniciando_mp_msg db     'Iniciando kernel (Modo Protegido)...'
+iniciando_mp_len equ    $ - iniciando_mp_msg
+
+;;
+;; Seccion de código.
+;; -------------------------------------------------------------------------- ;;
+
+;; Punto de entrada del kernel.
+BITS 16
+start:
+    ; Deshabilitar interrupciones
+    cli
+
+    ; Cambiar modo de video a 80 X 50
+    mov ax, 0003h
+    int 10h ; set mode 03h
+    xor bx, bx
+    mov ax, 1112h
+    int 10h ; load 8x8 font
+
+    ; Imprimir mensaje de bienvenida
+    imprimir_texto_mr iniciando_mr_msg, iniciando_mr_len, 0x07, 0, 0
+
+    ; Ejercicio 1
+    ; Habilitar A20
+    CALL habilitar_A20
+    ; Cargar la GDT
+    LGDT [GDT_DESC]
+    ; Setear el bit PE del registro CR0
+    MOV eax, cr0
+    or eax, 1
+    mov cr0, eax
+    ; Saltar a modo protegido
+    jmp 0x08:modoprotegido
+    BITS 32
+    modoprotegido:
+    ; Establecer selectores de segmentos
+    MOV AX, 0x48
+    MOV DS, AX
+    MOV AX, 0x48
+    MOV SS, AX
+    ; Establecer la base de la pila
+    MOV EBP, 0x27000
+    ; Imprimir mensaje de bienvenida
+    imprimir_texto_mp iniciando_mp_msg, iniciando_mp_len, 0x07, 0, 0
+
+    ; Ejercicio 2
+    ; Inicializar el juego
+
+    ; Inicializar pantalla
+
+    ; Inicializar el manejador de memoria
+
+    ; Inicializar el directorio de paginas
+
+    ; Cargar directorio de paginas
+
+    ; Habilitar paginacion
+
+    ; Inicializar tss
+
+    ; Inicializar tss de la tarea Idle
+
+    ; Inicializar el scheduler
+
+    ; Inicializar la IDT
+
+    ; Cargar IDT
+
+    ; Configurar controlador de interrupciones
+
+    ; Cargar tarea inicial
+
+    ; Habilitar interrupciones
+
+    ; Saltar a la primera tarea: Idle
+
+    ; Ciclar infinitamente (por si algo sale mal...)
+    mov eax, 0xFFFF
+    mov ebx, 0xFFFF
+    mov ecx, 0xFFFF
+    mov edx, 0xFFFF
+    jmp $
+    jmp $
+
+;; -------------------------------------------------------------------------- ;;
+
+%include "a20.asm"
